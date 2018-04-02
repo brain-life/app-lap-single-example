61619ada5362c93038e4b005af9de6dd3016111d
Giulia Berto <gberto@hm008.karst.uits.iu.edu>
2018-04-02 13:58:32 -0400

    test

--- lifeConverter.m
+++ lifeConverter.m
@@ -20,7 +20,20 @@ fe2trk(fe_src_static, ref_src_static, trk_out_static);
 % Convert segmentation
 load(fullfile(config.segmentation));
 tract_name_list = {}
-write_fg_to_trk(fg_classified(config.tract),ref_src_moving,sprintf('%s_tract.trk',strrep(fg_classified(config.tract).name,' ','_')));
+disp(config.tract1>0);
+disp(config.tract3>0);
+
+tract_name_list = [config.tract1]
+if (config.tract2>0)
+    disp("tract2 >0");
+    tract_name_list = [tract_name_list, config.tract2];
+end
+
+if config.tract4>0
+    disp("tract4 >0");
+end
+
+write_fg_to_trk(fg_classified(config.tract1),ref_src_moving,sprintf('%s_tract.trk',strrep(fg_classified(config.tract).name,' ','_')));
 
 exit;
 end
