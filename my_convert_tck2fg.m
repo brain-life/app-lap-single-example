a9b8b9abaf23527243f3f713a728beefb4c39351
Giulia Berto <gberto@i4.karst.uits.iu.edu>
2018-06-06 04:18:13 -0400

    set fiberPointStride=1

--- my_convert_tck2fg.m
+++ my_convert_tck2fg.m
@@ -4,7 +4,7 @@ if ~isdeployed
 	addpath(genpath('/N/u/brlife/git/vistasoft'));
 end
 
-fg_classified = dtiImportFibersMrtrix(tck_filename, 0.2, 0.5);
+fg_classified = dtiImportFibersMrtrix(tck_filename, 1);
 fgWrite(fg_classified, tract_name, 'mat');
 
 exit;
