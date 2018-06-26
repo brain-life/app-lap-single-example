1437c79e2e9589f42bb894df3241fc518f7b5252
Giulia Berto <gberto@i2.karst.uits.iu.edu>
2018-06-26 10:06:09 -0400

    increased njobs=16

--- compute_kdtree_and_dr_tractogram_0625.py
+++ compute_kdtree_and_dr_tractogram_0625.py
@@ -35,7 +35,7 @@ def compute_kdtree_and_dr_tractogram(tractogram, num_prototypes=None, distance=b
                                                          num_prototypes=num_prototypes,
                                                          distance=bundles_distances_mam,
                                                          prototype_policy='sff',
-                                                         n_jobs=8,
+                                                         n_jobs=16,
                                                          verbose=False)
     prototypes = tractogram[prototype_idx]
     print("Building the KD-tree of tractogram.")
